472d96d436b91f70203640d97805ba830e497afd
Mitesh <vmitesh9317@gmail.com>
2025-01-27 00:14:52 +0530

    update home page

--- Mitesh_Advanced_Responsive_HTML_With_Media_Query/Assignment/Doc/script.js
+++ Mitesh_Advanced_Responsive_HTML_With_Media_Query/Assignment/Doc/script.js
@@ -30,6 +30,8 @@ $(document).ready(function () {
   });
 });
 
+// second slider
+
 $(document).ready(function () {
   $(".slider2").owlCarousel({
     margin: 10,
@@ -57,6 +59,38 @@ $(document).ready(function () {
   });
 
   $("#nextBtn1").on("click", function () {
+    carousel.trigger("next.owl.carousel");
+    x;
+  });
+});
+
+// third slider
+$(document).ready(function () {
+  $(".slider3").owlCarousel({
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      500: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1500: {
+        items: 4,
+      },
+    },
+  });
+
+  const carousel = $(".slider3");
+
+  $("#prevBtn3").on("click", function () {
+    carousel.trigger("prev.owl.carousel");
+  });
+
+  $("#nextBtn3").on("click", function () {
     carousel.trigger("next.owl.carousel");
     x;
   });
